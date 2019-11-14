0dbbcfb5c62caaadc8a7e9ee340511527ef73afa
neo <xiongyu0523@gmail.com>
2019-11-14 19:00:00 +0800

    camera and lcd work as expected

---
+++ ili9341_driver/ili9341_ll.c
@@ -0,0 +1,375 @@
+#if defined(AzureSphere_CA7)
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "applibs_versions.h"
+
+#include <applibs/log.h>
+#include <applibs/i2c.h>
+#include <applibs/spi.h>
+#include <applibs/gpio.h>
+#include <hw/sample_hardware.h>
+
+static int RstGpioFd;
+static int DcGpioFd;
+static int BlGpioFd;
+static int i2cFd;
+static int spiFd;
+
+#define MAX_SPI_TRANSFER_BYTES	4096
+
+#elif defined(AzureSphere_CM4)
+
+#include <stdlib.h>
+
+#include "SPIMaster.h"
+#include "GPIO.h"
+#include "Log_Debug.h"
+
+#define MT3620_RDB_HEADER3_ISU3_SPI		MT3620_UNIT_ISU3
+#define MT3620_RDB_HEADER1_PIN4_GPIO	0
+#define MT3620_RDB_HEADER1_PIN6_GPIO	1
+#define MT3620_RDB_HEADER1_PIN8_GPIO	2
+
+static SpiMaster* SpiHandler;
+#define MAX_SPI_TRANSFER_BYTES	16
+
+#endif
+
+#include "ili9341_ll.h"
+
+void ili9341_ll_reset_low(void)
+{
+#if defined(AzureSphere_CA7)
+
+	GPIO_SetValue(RstGpioFd, GPIO_Value_Low);
+
+#elif defined(AzureSphere_CM4)
+
+	(void)GPIO_Write(MT3620_RDB_HEADER1_PIN4_GPIO, 0);
+
+#endif
+}
+void ili9341_ll_reset_high(void)
+{
+#if defined(AzureSphere_CA7)
+
+	GPIO_SetValue(RstGpioFd, GPIO_Value_High);
+
+#elif defined(AzureSphere_CM4)
+
+	(void)GPIO_Write(MT3620_RDB_HEADER1_PIN4_GPIO, 1);
+
+#endif
+}
+
+void ili9341_ll_dc_low(void)
+{
+#if defined(AzureSphere_CA7)
+
+	GPIO_SetValue(DcGpioFd, GPIO_Value_Low);
+
+#elif defined(AzureSphere_CM4)
+
+	(void)GPIO_Write(MT3620_RDB_HEADER1_PIN6_GPIO, 0);
+
+#endif
+}
+
+void ili9341_ll_dc_high(void)
+{
+#if defined(AzureSphere_CA7)
+
+	GPIO_SetValue(DcGpioFd, GPIO_Value_High);
+
+#elif defined(AzureSphere_CM4)
+
+	(void)GPIO_Write(MT3620_RDB_HEADER1_PIN6_GPIO, 1);
+
+#endif
+}
+
+void ili9341_ll_backlight_off(void)
+{
+#if defined(AzureSphere_CA7)
+
+	GPIO_SetValue(BlGpioFd, GPIO_Value_Low);
+
+#elif defined(AzureSphere_CM4)
+
+	(void)GPIO_Write(MT3620_RDB_HEADER1_PIN8_GPIO, 0);
+
+#endif
+}
+
+void ili9341_ll_backlight_on(void)
+{
+#if defined(AzureSphere_CA7)
+
+	GPIO_SetValue(BlGpioFd, GPIO_Value_High);
+
+#elif defined(AzureSphere_CM4)
+
+	(void)GPIO_Write(MT3620_RDB_HEADER1_PIN8_GPIO, 1);
+
+#endif
+}
+
+int ili9341_ll_init(void)
+{
+#if defined(AzureSphere_CA7)
+
+	RstGpioFd = GPIO_OpenAsOutput(ILI9341_RST, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (RstGpioFd < 0) {
+		Log_Debug("ERROR: GPIO_OpenAsOutput: errno=%d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	DcGpioFd = GPIO_OpenAsOutput(ILI9341_DC, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (DcGpioFd < 0) {
+		Log_Debug("ERROR: GPIO_OpenAsOutput: errno=%d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	BlGpioFd = GPIO_OpenAsOutput(ILI9341_BL, GPIO_OutputMode_PushPull, GPIO_Value_Low);
+	if (BlGpioFd < 0) {
+		Log_Debug("ERROR: GPIO_OpenAsOutput: errno=%d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	SPIMaster_Config config;
+	int ret = SPIMaster_InitConfig(&config);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_InitConfig: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	config.csPolarity = SPI_ChipSelectPolarity_ActiveLow;
+	spiFd = SPIMaster_Open(ILI9341_SPI, MT3620_SPI_CS_A, &config);
+	if (spiFd < 0) {
+		Log_Debug("ERROR: SPIMaster_Open: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	int result = SPIMaster_SetBusSpeed(spiFd, 8000000);
+	if (result < 0) {
+		Log_Debug("ERROR: SPIMaster_SetBusSpeed: errno=%d (%s)\r\n", errno, strerror(errno));
+		close(spiFd);
+		return -1;
+	}
+
+	result = SPIMaster_SetMode(spiFd, SPI_Mode_0);
+	if (result < 0) {
+		Log_Debug("ERROR: SPIMaster_SetMode: errno=%d (%s)\r\n", errno, strerror(errno));
+		close(spiFd);
+		return -1;
+	}
+
+	return 0;
+
+#elif defined(AzureSphere_CM4)
+
+	int32_t ret = GPIO_ConfigurePinForOutput(MT3620_RDB_HEADER1_PIN4_GPIO);
+	if (ret != ERROR_NONE) {
+		Log_Debug("ERROR: GPIO_ConfigurePinForOutput: %d\r\n", ret);
+		return -1;
+	}
+	GPIO_Write(MT3620_RDB_HEADER1_PIN4_GPIO, 1);
+
+	ret = GPIO_ConfigurePinForOutput(MT3620_RDB_HEADER1_PIN6_GPIO);
+	if (ret != ERROR_NONE) {
+		Log_Debug("ERROR: GPIO_ConfigurePinForOutput: %d\r\n", ret);
+		return -1;
+	}
+	GPIO_Write(MT3620_RDB_HEADER1_PIN6_GPIO, 1);
+
+	ret = GPIO_ConfigurePinForOutput(MT3620_RDB_HEADER1_PIN8_GPIO);
+	if (ret != ERROR_NONE) {
+		Log_Debug("ERROR: GPIO_ConfigurePinForOutput: %d\r\n", ret);
+		return -1;
+	}
+	GPIO_Write(MT3620_RDB_HEADER1_PIN8_GPIO, 0);
+
+	SpiHandler = SpiMaster_Open(MT3620_RDB_HEADER3_ISU3_SPI);
+	SpiMaster_ConfigDMA(SpiHandler, false);
+	SpiMaster_Select(SpiHandler, CS_LINE0);
+	SpiMaster_Configure(SpiHandler, false, false, 10000000);
+
+	return 0;
+
+#endif
+}
+
+int ili9341_ll_spi_tx_u8(uint8_t data)
+{
+#if defined(AzureSphere_CA7)
+
+	SPIMaster_Transfer transfers;
+
+	int ret = SPIMaster_InitTransfers(&transfers, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_InitTransfers: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	transfers.flags = SPI_TransferFlags_Write;
+	transfers.writeData = &data;
+	transfers.length = 1;
+
+	ret = SPIMaster_TransferSequential(spiFd, &transfers, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_TransferSequential: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	return 0;
+
+#elif defined(AzureSphere_CM4)
+
+	int32_t ret = SpiMaster_WriteSync(SpiHandler, &data, 1);
+	if (ret != ERROR_NONE) {
+		Log_Debug("ERROR: SpiMaster_WriteSync: %d\r\n", ret);
+		return -1;
+	}
+
+	return 0;
+
+#endif
+}
+
+int ili9341_ll_spi_rx_u8(uint8_t *data)
+{
+#if defined(AzureSphere_CA7)
+
+	SPIMaster_Transfer transfers;
+
+	int ret = SPIMaster_InitTransfers(&transfers, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_InitTransfers: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	transfers.flags = SPI_TransferFlags_Read;
+	transfers.readData = data;
+	transfers.length = 1;
+
+	ret = SPIMaster_TransferSequential(spiFd, &transfers, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_TransferSequential: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	return 0;
+
+#elif defined(AzureSphere_CM4)
+
+	int32_t ret = SpiMaster_ReadSync(SpiHandler, &data, 1);
+	if (ret != ERROR_NONE) {
+		Log_Debug("ERROR: SpiMaster_ReadSync: %d\r\n", ret);
+		return -1;
+	}
+
+	return 0;
+
+#endif
+}
+
+int ili9341_ll_spi_tx_u16(uint16_t data)
+{
+	uint8_t buf[2];
+	buf[0] = data >> 8;
+	buf[1] = data;
+
+#if defined(AzureSphere_CA7)
+
+	SPIMaster_Transfer transfers;
+
+	int ret = SPIMaster_InitTransfers(&transfers, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_InitTransfers: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	transfers.flags = SPI_TransferFlags_Write;
+	transfers.writeData = &buf[0];
+	transfers.length = 2;
+
+	ret = SPIMaster_TransferSequential(spiFd, &transfers, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_TransferSequential: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	return 0;
+
+#elif defined(AzureSphere_CM4)
+
+	int32_t ret = SpiMaster_WriteSync(SpiHandler, &buf, 2);
+	if (ret != ERROR_NONE) {
+		Log_Debug("ERROR: SpiMaster_WriteSync: %d\r\n", ret);
+		return -1;
+	}
+
+	return 0;
+
+#endif
+}
+
+int ili9341_ll_spi_tx(uint8_t *p_data, uint32_t tx_len)
+{
+#if defined(AzureSphere_CA7)
+
+	uint32_t numOfXfer = tx_len / MAX_SPI_TRANSFER_BYTES;
+	uint32_t lastXferSize = tx_len % MAX_SPI_TRANSFER_BYTES;
+
+	SPIMaster_Transfer transfer;
+	int ret = SPIMaster_InitTransfers(&transfer, 1);
+	if (ret < 0) {
+		Log_Debug("ERROR: SPIMaster_InitTransfers: errno=%d (%s)\r\n", errno, strerror(errno));
+		return -1;
+	}
+
+	transfer.flags = SPI_TransferFlags_Write;
+	transfer.length = MAX_SPI_TRANSFER_BYTES;
+
+	for (uint32_t i = 0; i < numOfXfer; i++) {
+
+		transfer.writeData = p_data;
+
+		ret = SPIMaster_TransferSequential(spiFd, &transfer, 1);
+		if (ret < 0) {
+			Log_Debug("ERROR: SPIMaster_TransferSequential: errno=%d (%s)\r\n", errno, strerror(errno));
+			return -1;
+		} else if (ret != transfer.length) {
+			Log_Debug("ERROR: SPIMaster_TransferSequential transfer %d bytes, expect %d bytes\r\n", ret, transfer.length);
+			return -1;
+		}
+
+		p_data += MAX_SPI_TRANSFER_BYTES;
+	}
+
+	if (lastXferSize > 0) {
+		transfer.writeData = p_data;
+		transfer.length = lastXferSize;
+
+		ret = SPIMaster_TransferSequential(spiFd, &transfer, 1);
+		if (ret < 0) {
+			Log_Debug("ERROR: SPIMaster_TransferSequential: errno=%d (%s)\r\n", errno, strerror(errno));
+			return -1;
+		}
+		else if (ret != transfer.length) {
+			Log_Debug("ERROR: SPIMaster_TransferSequential transfer %d bytes, expect %d bytes\r\n", ret, transfer.length);
+			return -1;
+		}
+	}
+
+	return 0;
+
+#elif defined(AzureSphere_CM4)
+
+#endif
+}
+
--- main.c
+++ main.c
@@ -1,43 +1,120 @@
-﻿#include <stdbool.h>
+﻿#include <stdlib.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
-#include <time.h>
 
 #include <applibs/log.h>
-#include <applibs/gpio.h>
+#include <time.h>
+#include <signal.h>
+#include <applibs/networking.h>
 
-int main(void)
+#include "ArduCAM.h"
+#include "ili9341.h"
+#include "delay.h"
+
+#define DISPLAY_WIDTH	128
+#define DISPLAY_HEIGHT	128
+
+static uint8_t s_CameraBuffer[320 * 240 * 2 + 8]; // QVGA from Camera 
+static uint8_t s_DisplayBuffer[DISPLAY_WIDTH * DISPLAY_HEIGHT * 2];    // 128x128 Centralized display on LCD
+static uint8_t s_Cifar10ResizeBuffer[32 * 32 * 3];// stanard cifar-10 format
+
+void resize_for_cifar10(uint8_t *p_in, uint8_t *p_out)
 {
-    // This minimal Azure Sphere app repeatedly toggles GPIO 9, which is the green channel of RGB
-    // LED 1 on the MT3620 RDB.
-    // If your device exposes different GPIOs, you might need to change this value. For example,
-    // to run the app on a Seeed mini-dev kit, change the GPIO from 9 to 7 in the call to
-    // GPIO_OpenAsOutput and in the app_manifest.json to blink its LED. Check with your hardware
-    // manufacturer to determine which GPIOs are available.
-    // Use this app to test that device and SDK installation succeeded that you can build,
-    // deploy, and debug an app with Visual Studio, and that you can deploy an app over the air,
-    // per the instructions here: https://docs.microsoft.com/azure-sphere/quickstarts/qs-overview
-    //
-    // It is NOT recommended to use this as a starting point for developing apps; instead use
-    // the extensible samples here: https://github.com/Azure/azure-sphere-samples
-    Log_Debug(
-        "\nVisit https://github.com/Azure/azure-sphere-samples for extensible samples to use as a "
-        "starting point for full applications.\n");
+	int resize_ratio = 4 * 2; // 128/32 = 4, 2 bytes per pixel
 
-    // Change the GPIO number here and in app_manifest.json if required by your hardware.
-    int fd = GPIO_OpenAsOutput(9, GPIO_OutputMode_PushPull, GPIO_Value_High);
-    if (fd < 0) {
-        Log_Debug(
-            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
-            strerror(errno), errno);
-        return -1;
-    }
+	for (int y = 0; y < 32; y++) {
+		for (int x = 0; x < 32; x++) {
 
-    const struct timespec sleepTime = {1, 0};
-    while (true) {
-        GPIO_SetValue(fd, GPIO_Value_Low);
-        nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(fd, GPIO_Value_High);
-        nanosleep(&sleepTime, NULL);
-    }
+			int orig_img_loc = y * 128 * resize_ratio + x * resize_ratio;
+			int out_img_loc = ((32 - 1 - y) * 32 + (32 - 1 - x)) * 3;
+			uint8_t pix_hi = p_in[orig_img_loc];
+			uint8_t pix_lo = p_in[orig_img_loc + 1];
+
+			p_out[out_img_loc] = (0xF8 & pix_hi);
+			p_out[out_img_loc + 1] = ((0x07 & pix_hi) << 5) | ((0xE0 & pix_lo) >> 3);
+			p_out[out_img_loc + 2] = (0x1F & pix_lo) << 3;
+#if 0
+			int orig_img_loc = y * 128 * resize_ratio + x * resize_ratio;
+			int out_img_loc = ((32 - 1 - y) * 32 + (32 - 1 - x)) * 2;
+			uint8_t pix_hi = p_in[orig_img_loc];
+			uint8_t pix_lo = p_in[orig_img_loc + 1];
+
+			p_out[out_img_loc] = p_in[orig_img_loc];
+			p_out[out_img_loc + 1] = p_in[orig_img_loc + 1];
+#endif
+		}
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	Log_Debug("Exmaple to capture a JPEG image from ArduCAM mini 2MP Plus and send to Azure Blob\r\n");
+
+	ili9341_init();
+
+	// init hardware and probe camera
+	arducam_ll_init();
+	arducam_reset();
+	if (arducam_test() == 0) {
+		Log_Debug("ArduCAM mini 2MP Plus is found\r\n");
+	}
+	else {
+		Log_Debug("ArduCAM mini 2MP Plus is not found\r\n");
+		return -1;
+	}
+
+	// config Camera
+
+	arducam_set_format(BMP);
+	arducam_InitCAM();
+	delay_ms(1000);
+	arducam_clear_fifo_flag();
+	arducam_flush_fifo();
+
+	uint16_t leftcorner_x = (320 - DISPLAY_WIDTH) / 2;
+	uint16_t leftcorner_y = (240 - DISPLAY_HEIGHT) / 2;
+
+	ili9341_draw_rect(leftcorner_x - 1, leftcorner_y - 1, DISPLAY_WIDTH + 2, DISPLAY_HEIGHT + 2, RED);
+
+	while (1) {
+
+		// Trigger a capture and wait for data ready in DRAM
+		arducam_start_capture();
+		while (!arducam_check_fifo_done());
+
+		uint32_t img_len = arducam_read_fifo_length();
+		if (img_len > MAX_FIFO_SIZE) {
+			Log_Debug("ERROR: FIFO overflow\r\n");
+			return -1;
+		}
+
+		arducam_CS_LOW();
+		arducam_set_fifo_burst();
+		arducam_read_fifo_burst(&s_CameraBuffer[0], img_len);
+		arducam_CS_HIGH();
+
+		arducam_clear_fifo_flag();
+
+		uint32_t r_pos = 320 * 2 * leftcorner_y + leftcorner_x * 2;
+		uint32_t w_pos = 0;
+		
+		for (uint8_t row = 0; row < DISPLAY_HEIGHT; row++) {
+			memcpy(&s_DisplayBuffer[w_pos], &s_CameraBuffer[r_pos], DISPLAY_WIDTH * 2);
+
+			w_pos += DISPLAY_WIDTH * 2;
+			r_pos += 640;
+		}
+
+		ili9341_draw_bitmap(leftcorner_x, leftcorner_y, DISPLAY_WIDTH, DISPLAY_HEIGHT, &s_DisplayBuffer[0]);
+
+		resize_for_cifar10(&s_DisplayBuffer[0], &s_Cifar10ResizeBuffer[0]);
+
+		// pass to M4 core to do NN inference
+	}
+
+	Log_Debug("App Exit\r\n");
+
+	return 0;
 }
