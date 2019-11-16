f22cf130dc0e2e6a8b7c52a766d7030b54bfe97a
neo <xiongyu0523@gmail.com>
2019-11-16 17:41:16 +0800

    First demo

--- main.c
+++ main.c
@@ -6,51 +6,95 @@
 #include <applibs/log.h>
 #include <time.h>
 #include <signal.h>
-#include <applibs/networking.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <applibs/application.h>
 
+#include "epoll_timerfd_utilities.h"
 #include "ArduCAM.h"
 #include "ili9341.h"
 #include "delay.h"
 
+static void SocketEventHandler(EventData* eventData);
+
+static const char rtAppComponentId[] = "6583cf17-d321-4d72-8283-0b7c5b56442b";
+
 #define DISPLAY_WIDTH	128
 #define DISPLAY_HEIGHT	128
+#define DISPLAY_DEPTH	2
+
+#define CFIAR10_WIDTH	32
+#define CFIAR10_HEIGHT  32
+#define CFIAR10_DEPTH   3
 
 static uint8_t s_CameraBuffer[320 * 240 * 2 + 8]; // QVGA from Camera 
-static uint8_t s_DisplayBuffer[DISPLAY_WIDTH * DISPLAY_HEIGHT * 2];    // 128x128 Centralized display on LCD
-static uint8_t s_Cifar10ResizeBuffer[32 * 32 * 3];// stanard cifar-10 format
+static uint8_t s_DisplayBuffer[DISPLAY_WIDTH * DISPLAY_HEIGHT * DISPLAY_DEPTH];       // 128x128 Centralized display on LCD
+static uint8_t s_Cifar10ResizeBuffer[CFIAR10_WIDTH * CFIAR10_HEIGHT * CFIAR10_DEPTH]; // stanard cifar-10 format
 
-void resize_for_cifar10(uint8_t *p_in, uint8_t *p_out)
+static int epollFd = 0;
+static int rtSocketFd = 0;
+static EventData socketEventData = { .eventHandler = &SocketEventHandler };
+
+static void SocketEventHandler(EventData* eventData)
 {
-	int resize_ratio = 4 * 2; // 128/32 = 4, 2 bytes per pixel
+	const char* cifar10_label[] = { "Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck" };
+
+	uint8_t label;
+	ssize_t bytesReceived = recv(rtSocketFd, &label, sizeof(label), 0);
+	if (bytesReceived < 0) {
+		Log_Debug("ERROR: Unable to receive message: %d (%s)\r\n", errno, strerror(errno));
+		//terminationRequired = true;
+		return;
+	}
+
+	Log_Debug("This is a %s\r\n", cifar10_label[label]);
+}
+
+void resize(uint8_t *p_in, uint8_t *p_out)
+{
+	int resize_ratio = 4 * 2; // 128/32 = 4 x 2 bytes per pixel
 
 	for (int y = 0; y < 32; y++) {
 		for (int x = 0; x < 32; x++) {
 
 			int orig_img_loc = y * 128 * resize_ratio + x * resize_ratio;
-			int out_img_loc = ((32 - 1 - y) * 32 + (32 - 1 - x)) * 3;
+			int out_img_loc = (y * 32 + x) * 3;
+
 			uint8_t pix_hi = p_in[orig_img_loc];
 			uint8_t pix_lo = p_in[orig_img_loc + 1];
 
 			p_out[out_img_loc] = (0xF8 & pix_hi);
 			p_out[out_img_loc + 1] = ((0x07 & pix_hi) << 5) | ((0xE0 & pix_lo) >> 3);
 			p_out[out_img_loc + 2] = (0x1F & pix_lo) << 3;
-#if 0
-			int orig_img_loc = y * 128 * resize_ratio + x * resize_ratio;
-			int out_img_loc = ((32 - 1 - y) * 32 + (32 - 1 - x)) * 2;
-			uint8_t pix_hi = p_in[orig_img_loc];
-			uint8_t pix_lo = p_in[orig_img_loc + 1];
-
-			p_out[out_img_loc] = p_in[orig_img_loc];
-			p_out[out_img_loc + 1] = p_in[orig_img_loc + 1];
-#endif
 		}
 	}
 }
 
-
 int main(int argc, char* argv[])
 {
 	Log_Debug("Exmaple to capture a JPEG image from ArduCAM mini 2MP Plus and send to Azure Blob\r\n");
+
+	epollFd = CreateEpollFd();
+	if (epollFd < 0) {
+		return -1;
+	}
+
+	rtSocketFd = Application_Socket(rtAppComponentId);
+	if (rtSocketFd == -1) {
+		Log_Debug("ERROR: Unable to create socket: %d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	struct timeval to = { .tv_sec = 1, .tv_usec = 0 };
+	int ret = setsockopt(rtSocketFd, SOL_SOCKET, SO_RCVTIMEO, &to, sizeof(to));
+	if (ret == -1) {
+		Log_Debug("ERROR: Unable to set socket timeout: %d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	if (RegisterEventHandlerToEpoll(epollFd, rtSocketFd, &socketEventData, EPOLLIN) != 0) {
+		return -1;
+	}
 
 	ili9341_init();
 
@@ -99,7 +143,7 @@ int main(int argc, char* argv[])
 
 		uint32_t r_pos = 320 * 2 * leftcorner_y + leftcorner_x * 2;
 		uint32_t w_pos = 0;
-		
+
 		for (uint8_t row = 0; row < DISPLAY_HEIGHT; row++) {
 			memcpy(&s_DisplayBuffer[w_pos], &s_CameraBuffer[r_pos], DISPLAY_WIDTH * 2);
 
@@ -109,9 +153,18 @@ int main(int argc, char* argv[])
 
 		ili9341_draw_bitmap(leftcorner_x, leftcorner_y, DISPLAY_WIDTH, DISPLAY_HEIGHT, &s_DisplayBuffer[0]);
 
-		resize_for_cifar10(&s_DisplayBuffer[0], &s_Cifar10ResizeBuffer[0]);
+		resize(&s_DisplayBuffer[0], &s_Cifar10ResizeBuffer[0]);
 
-		// pass to M4 core to do NN inference
+		// max allowed size is 1KB for a single transfer
+		const uint32_t maxInterCoreBufSize = 1024;
+		for (uint32_t i = 0; i < sizeof(s_Cifar10ResizeBuffer) / maxInterCoreBufSize; i++) {
+			ssize_t bytesSent = send(rtSocketFd, &s_Cifar10ResizeBuffer[i * maxInterCoreBufSize], maxInterCoreBufSize, 0);
+			if (bytesSent < 0) {
+				Log_Debug("ERROR: Unable to send message: %d (%s)\r\n", errno, strerror(errno));
+			} else if (bytesSent != maxInterCoreBufSize) {
+				Log_Debug("ERROR: Write %d bytes, expect %d bytes\r\n", bytesSent, maxInterCoreBufSize);
+			}
+		}
 	}
 
 	Log_Debug("App Exit\r\n");
